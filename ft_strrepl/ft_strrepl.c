726bc87fa998b192576f930abc524d97a66778de
Exam 42 <exam@u91z06s04.42kl.edu.my>
2021-07-16 15:21:53 +0800

    strreprl

--- ft_strrepl/ft_strrepl.c
+++ ft_strrepl/ft_strrepl.c
@@ -1,5 +1,3 @@
-char	g_res[2048];
-
 char	*ft_strrepl(char *str, char c1, char c2)
 {
 	int	i;
@@ -8,12 +6,8 @@ char	*ft_strrepl(char *str, char c1, char c2)
 	while (str[i])
 	{
 		if (str[i] == c1)
-			g_res[i] = c2;
-		else
-			g_res[i] = str[i];
+			str[i] = c2;
 		i++;
 	}
-	g_res[i] = 0;
-	str = g_res;
 	return (str);
 }
